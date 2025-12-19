44111d50fc8d9ac8b8d48b387da709a8934c0690
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-12-19 15:24:52 +0300

    update settings.py

--- aerca_backend/settings.py
+++ aerca_backend/settings.py
@@ -166,17 +166,28 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
 ]
 CORS_ALLOW_CREDENTIALS = True
-SESSION_COOKIE_NAME = "sessionid"
-SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SAMESITE = "None"  # Lax works in localhost, None requires HTTPS
-SESSION_COOKIE_SECURE = True     # True only if using HTTPS
+# SESSION_COOKIE_NAME = "sessionid"
+# SESSION_COOKIE_HTTPONLY = True
+# SESSION_COOKIE_SAMESITE = "None"  # Lax works in localhost, None requires HTTPS
+# SESSION_COOKIE_SECURE = True     # True only if using HTTPS
 
-# CSRF cookies
-CSRF_COOKIE_NAME = "csrftoken"
-CSRF_COOKIE_HTTPONLY = False      # Must be readable by JS
-CSRF_COOKIE_SAMESITE = "Lax"
-CSRF_COOKIE_SECURE = True
+# # CSRF cookies
+# CSRF_COOKIE_NAME = "csrftoken"
+# CSRF_COOKIE_HTTPONLY = False      # Must be readable by JS
+# CSRF_COOKIE_SAMESITE = "Lax"
+# CSRF_COOKIE_SECURE = True
 
 # Trusted origins for CSRF
+if DEBUG:
+    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SAMESITE = "Lax"
+    CSRF_COOKIE_SECURE = False
+else:
+    SESSION_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SAMESITE = "None"
+    CSRF_COOKIE_SECURE = True
+
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000",
 "http://localhost:3001"]
