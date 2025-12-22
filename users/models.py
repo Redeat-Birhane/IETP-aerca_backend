1cac1254ea7c8d0a4a4c37696436ab94658f8cba
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-12-22 10:34:47 +0300

    update location for taxworkers and rating for transitors

--- users/models.py
+++ users/models.py
@@ -75,6 +75,8 @@ class TaxWorkerProfile(models.Model):
     earning = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     organization_name = models.CharField(max_length=10)
     years_of_experience = models.IntegerField(default = 0)
+    location = models.CharField(max_length=255, blank=True, null=True)
+
 
     def __str__(self):
         return f"Taxworker: {self.user.username}"
